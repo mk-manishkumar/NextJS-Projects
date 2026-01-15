dd5d421af3f831b3f6dec995bb6e149ab4018e64
Manish Kumar <manish.login01@gmail.com>
2026-01-15 22:11:57 +0530

    fix(route ): update slug redirect handler to async params

--- trimbyte/src/app/[slug]/route.ts
+++ trimbyte/src/app/[slug]/route.ts
@@ -3,23 +3,34 @@ import { connectDB } from "@/lib/db";
 import UserLink from "@/models/UserLink.model";
 import AnonymousLink from "@/models/AnonymousLink.model";
 
-export const GET = async (req: NextRequest, { params }: { params: { slug: string } }) => {
+export const GET = async (
+  req: NextRequest,
+  context: { params: Promise<{ slug: string }> }
+) => {
   try {
     await connectDB();
 
-    const { slug } = params;
+    const { slug } = await context.params;
 
-    const userLink = await UserLink.findOneAndUpdate({ slug }, { $inc: { clicks: 1 } }, { new: true });
+    const userLink = await UserLink.findOneAndUpdate(
+      { slug },
+      { $inc: { clicks: 1 } },
+      { new: true }
+    );
 
     if (userLink) return NextResponse.redirect(userLink.originalUrl);
-
+    
     const anonymousLink = await AnonymousLink.findOne({ slug });
 
     if (anonymousLink) return NextResponse.redirect(anonymousLink.originalUrl);
+    
 
-    return NextResponse.json({ message: "Link not found" }, { status: 404 });
+    return NextResponse.json(
+      { message: "Link not found" },
+      { status: 404 }
+    );
   } catch (error) {
     if (process.env.NODE_ENV === "development") console.error(error);
-    return NextResponse.json({ message: "Internal server error" }, { status: 500 });
+    return NextResponse.json({ message: "Internal server error" },{ status: 500 });
   }
 };
